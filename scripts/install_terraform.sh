0d6c8a98d8e75ab81d36316b84fd90e6feb29190
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 18:43:49 +0530

    ./terraform added

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -14,5 +14,7 @@ sudo apt-get install wget curl -y
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
-
+cd ~/usr/bin/terraform
+ls -a
+# chmod +x binary_file_name
 terraform -v
