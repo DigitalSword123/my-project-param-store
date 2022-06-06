c2e021174dea58e36b2291c11d60868fea51aee0
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 17:18:58 +0530

    modified command

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -1,5 +1,7 @@
+echo $PATH
 sudo apt-get install wget unzip -y
 wget http://releases.hashicorp.com/terraform/0.12.5/terraform_0.12.5_linux_amd64.zip
-unzip terraform_0.12.5_linux_amd64.zip
+sudo unzip ./ terraform_0.12.5_linux_amd64.zip –d /usr/local/bin
+terraform -v
 
-sudo mv terraform /usr/local/bin/
+# sudo mv terraform /usr/local/bin/
