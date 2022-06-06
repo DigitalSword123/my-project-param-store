f03863503543a709ab7162a3396db33cbdf50ba7
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 20:34:46 +0530

    modified

--- scripts/terraform_apply.sh
+++ scripts/terraform_apply.sh
@@ -1,10 +1,9 @@
 set -e
 
 echo "Fetching values files"
-cd ../vars
-$a=git ls-files
-    foreach ($i in $a) {
-        cd ..
-        terraform apply -var-file=vars/i
-        echo "printing"$i
-    }
+cd ./vars
+for FILE in *
+    do
+     terraform apply -var-file=$FILE
+     echo "printing"$FILE
+    done
--- scripts/terraform_plan.sh
+++ scripts/terraform_plan.sh
@@ -2,9 +2,15 @@ set -e
 
 echo "Fetching values files"
 cd ./vars
-$a=git ls-files
-    foreach ($i in $a) {
-        cd ..
-        terraform plan -var-file=vars/i
-        echo "printing"$i
-    }
+for FILE in *
+    do
+     terraform plan -var-file=$FILE
+     echo "printing"$FILE
+    done
+
+    
+    # foreach ($i in $a) {
+    #     cd ..
+    #     terraform plan -var-file=vars/i
+    #     echo "printing"$i
+    # }
