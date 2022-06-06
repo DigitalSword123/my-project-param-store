248f2f3aeacb466049cf0bce5cb1ad081aa337bb
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 18:33:59 +0530

    ./terraform added

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -1,12 +1,14 @@
+echo $PATH
+# sudo apt-get update
+sudo apt install software-properties-common -y
 sudo apt-get install wget unzip -y
-wget http://releases.hashicorp.com/terraform/0.12.5/terraform_0.12.5_linux_amd64.zip
-unzip terraform_0.12.5_linux_amd64.zip
+# wget http://releases.hashicorp.com/terraform/0.12.5/terraform_0.12.5_linux_amd64.zip
+# unzip terraform_0.12.5_linux_amd64.zip
+# sudo mv terraform /usr/local/bin/
 
-sudo mv terraform /usr/local/bin/
-
-# sudo apt-get install wget curl -y
-# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-# sudo apt-get update && sudo apt-get install terraform
+sudo apt-get install wget curl -y
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
 
 terraform -v
--- scripts/terraform_apply.sh
+++ scripts/terraform_apply.sh
@@ -5,6 +5,6 @@ cd ../vars
 $a=git ls-files
     foreach ($i in $a) {
         cd ..
-        terraform apply -var-file=vars/i
+        ./terraform apply -var-file=vars/i
         echo "printing"$i
     }
