d072f620cedd92368d93f17caa6a50451f54a4a8
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 19:09:54 +0530

    C:/Program Files/Git/ added in usr/bin

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -7,13 +7,20 @@ echo $PATH
 # sudo mv terraform /usr/local/bin/
 
 # newer version of terraform
-sudo apt-get update
-sudo apt install software-properties-common -y
+# sudo apt-get update
+# sudo apt install software-properties-common -y
+# sudo apt-get install wget unzip -y
+# sudo apt-get install wget curl -y
+# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+# sudo apt-get update && sudo apt-get install terraform
+
+# older version of terraform
 sudo apt-get install wget unzip -y
-sudo apt-get install wget curl -y
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+wget https://releases.hashicorp.com/terraform/1.0.5/terraform_1.0.5_linux_amd64.zip
+unzip terraform_1.0.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
 cd /usr/bin/terraform
 ls -a
 # chmod +x binary_file_name
