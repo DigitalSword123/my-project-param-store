03395c27c350b03b51ffb13b6efa1f0c58d72402
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 20:18:54 +0530

    added

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -18,10 +18,12 @@ echo $PATH
 # older version of terraform
 sudo apt-get install gfortran
 sudo apt-get install wget unzip -y
-wget https://releases.hashicorp.com/terraform/1.0.5/terraform_1.0.5_linux_amd64.zip
-unzip terraform_1.0.5_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/1.2.2/terraform_1.2.2_windows_amd64.zip
+unzip terraform_1.2.2_windows_amd64.zip
 ls -a
 chmod +x terraform
+file terraform
+uname -a
 
 
 sudo mv terraform /usr/local/bin
