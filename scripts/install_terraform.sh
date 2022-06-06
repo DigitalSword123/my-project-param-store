c594154db75b6b6f14597216e9243704b0b9b2fa
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 20:02:28 +0530

    added 32 bit terraform

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -17,10 +17,10 @@ echo $PATH
 
 # older version of terraform
 sudo apt-get install wget unzip -y
-wget https://releases.hashicorp.com/terraform/1.0.5/terraform_1.0.5_linux_amd64.zip
-unzip terraform_1.0.5_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/1.0.5/terraform_1.0.5_linux_386.zip
+unzip terraform_1.0.5_linux_386.zip
 ls -a
-chmod +x terraform
+# chmod +x terraform
 
 sudo mv terraform /usr/local/bin
 
