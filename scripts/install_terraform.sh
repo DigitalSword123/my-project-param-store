50b7a7bb331519d66d8a5d09b913afcdbff0e748
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 18:48:47 +0530

    C:/Program Files/Git/ added in usr/bin

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -7,14 +7,14 @@ echo $PATH
 # sudo mv terraform /usr/local/bin/
 
 # newer version of terraform
-sudo apt-get update
+# sudo apt-get update
 sudo apt install software-properties-common -y
 sudo apt-get install wget unzip -y
 sudo apt-get install wget curl -y
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
-cd ~/usr/bin/terraform
+cd /usr/bin/terraform
 ls -a
 # chmod +x binary_file_name
 terraform -v
