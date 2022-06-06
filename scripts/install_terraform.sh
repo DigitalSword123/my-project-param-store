b5621bb21ea7a718b383443e8a1ff1f26ad8e65f
Amiya Rana <ranaamiya70@gmail.com>
2022-06-06 18:36:45 +0530

    ./terraform added

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -1,11 +1,15 @@
 echo $PATH
-# sudo apt-get update
-sudo apt install software-properties-common -y
-sudo apt-get install wget unzip -y
+
+# older version of terraform
+# sudo apt-get install wget unzip -y
 # wget http://releases.hashicorp.com/terraform/0.12.5/terraform_0.12.5_linux_amd64.zip
 # unzip terraform_0.12.5_linux_amd64.zip
 # sudo mv terraform /usr/local/bin/
 
+# newer version of terraform
+sudo apt-get update
+sudo apt install software-properties-common -y
+sudo apt-get install wget unzip -y
 sudo apt-get install wget curl -y
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
