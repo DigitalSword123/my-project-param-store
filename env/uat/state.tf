f53979d2c7b9ef1bf38d1e919ec81cfd3f3c5568
Amiya Rana <ranaamiya70@gmail.com>
2022-06-07 18:30:30 +0530

    state file modified

--- env/uat/state.tf
+++ env/uat/state.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket  = "employee-data-node-terraform-state-bucket"
     encrypt = false
-    key     = "param-store/${var.env}/terraform.tfstate"
+    key     = "param-store/uat/terraform.tfstate"
     region  = "ap-south-1"
     access_key = "AKIAZ332BW4JK4JM2BMC"
     secret_key = "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"
