4089c92f777c5ca935751db5cc83937d385c8817
Amiya Rana <ranaamiya70@gmail.com>
2022-06-13 19:13:02 +0530

    lower var.env

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 
 resource "aws_ssm_parameter" "parameters" {
   for_each    = var.ssm-infra-key-value-map
-  name        = "${var.ssm-path}/${var.env}/${each.key}"
+  name        = "${var.ssm-path}/${lower(var.env)}/${each.key}"
   description = element(each.value, 2)
   type        = element(each.value, 0)
   value       = element(each.value, 1)
