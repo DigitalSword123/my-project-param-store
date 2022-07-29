e8b896996646503004e404f5e5003772c7ef3dbf
Amiya Rana <ranaamiya70@gmail.com>
2022-07-30 00:45:30 +0530

    test 4

--- state.tf
+++ state.tf
@@ -1,13 +1,9 @@
-
 # tis .tfstate file in s3 bucket will store already provisioned
 # infrastrucrures like lambda, bucket which are created previous terraform
-# apply command
 terraform {
   backend "s3" {
-    bucket     = "employee-data-node-terraform-state-bucket"
+    bucket     = "node-terraform-state-bucket"
     encrypt    = false
-    key        = "param-store/dev/terraform.tfstate"
     region     = "ap-south-1"
   }
 }
-
